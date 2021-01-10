2b3027267ebf3a93f58dd4ae0bda4ed841ac0e72
sdtblck <46172032+sdtblck@users.noreply.github.com>
2021-01-10 23:56:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setuptools.setup(
     version='1.0',
     scripts=['./scripts/depoison'],
     author='Sid Black',
+    url='https://github.com/EleutherAI/depoison/',
     description='Fixes poisoned directories in google cloud buckets',
     packages=['lib.depoison'],
     install_requires=['gsutil'],
